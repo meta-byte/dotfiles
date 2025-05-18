9df91236e1f9d538aa7f5d93124d907869fea2ad
meta-byte <45606335+meta-byte@users.noreply.github.com>
2025-05-18 10:57:59 -0600

    add trouble plugin

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,37 +1,46 @@
 return {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim"},
-    config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          -- "omnisharp",  -- C#
-          "gopls",      -- Go
-          "ts_ls",      -- JavaScript/TypeScript
-          "pyright",    -- Python
-          "jsonls",   -- JSON
-          "eslint", -- Optional: Linter for JavaScript/TypeScript
-          "lua_ls",     -- Lua (note: renamed from sumneko_lua to lua_ls)
-        },
-      })
-  
-      local lspconfig = require("lspconfig")
-      
-      -- Load custom LSP configurations
-      local function get_server_config(server_name)
-        local ok, server_config = pcall(require, "config.lsp." .. server_name)
-        if ok then
-          return server_config
-        else
-          return {}
-        end
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = { 
+    "neovim/nvim-lspconfig", 
+    "williamboman/mason.nvim"
+  },
+  config = function()
+    -- First, set up mason
+    require("mason").setup()
+    
+    -- Then set up mason-lspconfig with your servers
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        -- "omnisharp",  -- C#
+        "gopls",      -- Go
+        "ts_ls",   -- JavaScript/TypeScript
+        "pyright",    -- Python
+        "jsonls",     -- JSON
+        "eslint",     -- Optional: Linter for JavaScript/TypeScript
+        "lua_ls",     -- Lua (note: renamed from sumneko_lua to lua_ls)
+      },
+    })
+    
+    -- Define local variables
+    local lspconfig = require("lspconfig")
+    local mason_lspconfig = require("mason-lspconfig")
+   
+    -- Load custom LSP configurations
+    local function get_server_config(server_name)
+      local ok, server_config = pcall(require, "config.lsp." .. server_name)
+      if ok then
+        return server_config
+      else
+        return {}
       end
-  
-      require("mason-lspconfig").setup_handlers({
-        function(server_name) -- Default setup for all servers
-          local opts = get_server_config(server_name)
-          lspconfig[server_name].setup(opts)
-        end,
-      })
-    end,
-  }
+    end
+    
+    -- Set up handlers - using the local variable, not requiring the module again
+    mason_lspconfig.setup_handlers({
+      function(server_name) -- Default setup for all servers
+        local opts = get_server_config(server_name)
+        lspconfig[server_name].setup(opts)
+      end,
+    })
+  end,
+}
