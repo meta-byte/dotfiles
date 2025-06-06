cf69f4fb9a9f2a4f3e0cc381d03757a5a9e8fdb9
meta-byte <45606335+meta-byte@users.noreply.github.com>
2025-06-06 14:36:04 -0600

    add telescope UI plugin, simplify LSP config, setup lsp keybinds

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,64 +1,43 @@
 return {
-  "williamboman/mason-lspconfig.nvim",
-  dependencies = {
-    "neovim/nvim-lspconfig",
-    "williamboman/mason.nvim"
-  },
-  config = function()
-    -- First, set up mason
-    require("mason").setup()
-
-    -- Then set up mason-lspconfig with your servers
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        -- "omnisharp",  -- C#
-        "gopls",      -- Go
-        "ts_ls",      -- JavaScript/TypeScript (use tsserver instead of ts_ls)
-        "pyright",    -- Python
-        "jsonls",     -- JSON
-        "eslint",     -- Linter for JavaScript/TypeScript
-        "lua_ls",     -- Lua (note: renamed from sumneko_lua to lua_ls)
-      },
-      -- Handler functions for each server
-      handlers = {
-        gopls = function()
-          local opts = require("config.lsp.gopls") or {}
-          require("lspconfig").gopls.setup(opts)
-        end,
-        ts_ls = function()
-          local opts = require("config.lsp.ts_ls") or {}
-          require("lspconfig").ts_ls.setup(opts)
-        end,
-        pyright = function()
-          local opts = require("config.lsp.pyright") or {}
-          require("lspconfig").pyright.setup(opts)
-        end,
-        jsonls = function()
-          local opts = require("config.lsp.jsonls") or {}
-          require("lspconfig").jsonls.setup(opts)
-        end,
-        eslint = function()
-          local opts = require("config.lsp.eslint") or {}
-          require("lspconfig").eslint.setup(opts)
-        end,
-        lua_ls = function()
-          local opts = require("config.lsp.lua_ls") or {}
-          require("lspconfig").lua_ls.setup(opts)
-        end,
-      },
-    })
-    -- Define local variables
-    local lspconfig = require("lspconfig")
-    local mason_lspconfig = require("mason-lspconfig")
-
-    -- Load custom LSP configurations
-    local function get_server_config(server_name)
-      local ok, server_config = pcall(require, "config.lsp." .. server_name)
-      if ok then
-        return server_config
-      else
-        return {}
-      end
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
     end
-  end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+        -- "omnisharp",   -- C#
+        "gopls",          -- Go
+        "ts_ls",          -- JavaScript/TypeScript (use tsserver instead of ts_ls)
+        "pyright",        -- Python
+        "jsonls",         -- JSON
+        "eslint",         -- Linter for JavaScript/TypeScript
+        "lua_ls",         -- Lua (note: renamed from sumneko_lua to lua_ls)
+      },
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.gopls.setup({})
+      lspconfig.ts_ls.setup({})
+      lspconfig.eslint.setup({})
+      lspconfig.jsonls.setup({})
+      lspconfig.pyright.setup({})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+      vim.keymap.set('n', '<leader>gr', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
 }
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,22 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
-  config = function()
-    require("telescope").setup()
-    local TelescopeKeymaps = require("keymaps.telescope")
-    TelescopeKeymaps.setup()
-  end
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
+    config = function()
+      --This might be a legacy way of doing this
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      local TelescopeKeymaps = require("keymaps.telescope")
+      TelescopeKeymaps.setup()
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
